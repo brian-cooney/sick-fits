030915d951fab52d4a816b14506d78abe253c840
brian-cooney <bcooney@gmail.com>
2018-12-22 17:35:26 +0000

    First commit

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,8 @@
+class Home extends React.Component {
+  state = {};
+  render() {
+    return <h1>Hett</h1>;
+  }
+}
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,8 @@
+class Sell extends React.Component {
+  state = {};
+  render() {
+    return <h1>sell</h1>;
+  }
+}
+
+export default Sell;
