1dea803e3c0f89aa48bfba199d01e92a38698597
brian-cooney <bcooney@gmail.com>
2019-01-03 13:54:05 +0000

    Show current logged in user video 26

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,9 +1,24 @@
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
 
-const server = createServer(cookieParser());
+const server = createServer();
+
+server.express.use(cookieParser());
+
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
@@ -13,6 +28,6 @@ server.start(
     }
   },
   deets => {
-    console.log(`Server is running on port http://localhost:${deets.port}`);
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
